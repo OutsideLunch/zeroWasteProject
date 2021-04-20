e6089a5fa424a4efbc08a687e7dba0d906762a1e
HOJIN <ghrjf3018@gmail.com>
2021-04-21 04:01:14 +0900

    마이페이지 - 상품후기등록 1차업로드

--- src/main/java/com/bitc/zero/controller/MypageController.java
+++ src/main/java/com/bitc/zero/controller/MypageController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.bitc.zero.dto.MyPageDto;
@@ -19,15 +22,29 @@ import com.bitc.zero.service.ZeroService;
 @Controller
 public class MypageController {
 	
+	protected static final Logger logger = LogManager.getLogger(MypageController.class);
+	
 	@Autowired
 	private ZeroService zeroService;
+	
+	@Autowired
+	protected SqlSession sql;
 	
 	//	마이페이지 -> 주문목록
 		@RequestMapping(value="/zero/getMyPageList", method=RequestMethod.GET)
 		public ModelAndView myPage(HttpServletRequest req) throws Exception{
 			ModelAndView mv = new ModelAndView();
-			
 			HttpSession session = req.getSession();
+			try {
+				if(session.getAttribute("customerPk").toString() == null) {
+					mv.setViewName("/zero/main");
+					
+					return mv;
+				}
+			} catch (Exception e) {
+				logger.warn("::::session error");
+			}
+			
 			int customerPk =  (int) session.getAttribute("customerPk");
 			
 			List<MyPageDto> mypageInfoList = zeroService.getMypageInfo(customerPk);
@@ -40,10 +57,21 @@ public class MypageController {
 		}
 		
 		// 마이페이지 -> 상품후기등록
+		@ResponseBody
 		@RequestMapping(value="/zero/saveProductReview", method=RequestMethod.POST)
-		public String postProductReview(ReviewDto review, MultipartHttpServletRequest uploadFiles) throws Exception {
-			zeroService.postProductReview(review, uploadFiles);
+		public String postProductReview(ReviewDto dto, HttpServletRequest req) throws Exception {
+			//MultipartHttpServletRequest uploadFiles
+			//zeroService.postProductReview(dto, uploadFiles);
 			
-			return "redirect:/zero/myPage";
+			HttpSession session = req.getSession();
+			dto.setCustomerPk((int)session.getAttribute("customerPk"));
+			logger.info("param1 ::"+dto.getProductReviewContents());
+			logger.info("param2 ::"+dto.getProductReviewScore());
+			logger.info("param3 ::"+dto.getOrderDetailPk());
+			logger.info("param4 ::"+dto.getCustomerPk());
+			
+			sql.insert("myPageMapper.insertProductReview", dto);
+			
+			return "1";
 		}
 }
--- src/main/java/com/bitc/zero/dto/ReviewDto.java
+++ src/main/java/com/bitc/zero/dto/ReviewDto.java
@@ -1,16 +1,19 @@
 package com.bitc.zero.dto;
 
+import java.util.List;
+
 import lombok.Data;
 
 @Data
 public class ReviewDto {
 
 	// 후기 입력 출력
-	private String productReviewTitle;
-	private String customerName;
+	private int customerPk;
 	private String productReviewContents;
-	private String storedFilePath;
 	private int productReviewPk;
 	private int orderDetailPk;
 	private int productReviewScore;
+	List<TFileDto> file;
+	
+	
 }
