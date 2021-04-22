1b735b26650fab9657e823b69a3d54ea28dacd1c
ladybird17 <modalon@naver.com>
2021-04-22 21:39:54 +0900

    OrderController 수정

--- src/main/java/com/bitc/zero/controller/OrderController.java
+++ src/main/java/com/bitc/zero/controller/OrderController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +17,11 @@ import org.springframework.web.servlet.ModelAndView;
 import com.bitc.zero.dto.JoinDto;
 import com.bitc.zero.dto.OrderDto;
 import com.bitc.zero.dto.ProductDetailDto;
-import com.bitc.zero.service.ZeroService;
 
 @Controller
 public class OrderController {
 	@Autowired
-	private ZeroService zeroService;
+	protected SqlSession sql;
 	
 	//주문 페이지 가져오기
 	@RequestMapping(value="/zero/order", method=RequestMethod.GET)
@@ -29,7 +29,7 @@ public class OrderController {
 		ModelAndView mv = new ModelAndView("/zero/order");
 		
 		//product_pk, stored_file_path, product_name 가져옴
-		ProductDetailDto pd = zeroService.selectProductDetail(productPk);
+		ProductDetailDto pd = sql.selectOne("productMapper.selectProductDetail",productPk);
 		mv.addObject("pData", pd);
 		
 		//주문 수량, 총 주문금액
@@ -41,7 +41,7 @@ public class OrderController {
 		String customerEmail = (String)session.getAttribute("customerEmail");
 		
 		//회원 정보가 있는 JoinDto에서 현재 로그인한 고객의 customer_pk, name, phone, addr을 가져옴
-		JoinDto data = zeroService.selectCustomerInfo(customerEmail);
+		JoinDto data = sql.selectOne("commonMapper.selectCustomerInfo",customerEmail);
 		mv.addObject("cData", data);
 		
 		return mv;
@@ -57,13 +57,13 @@ public class OrderController {
 		order.setOrderCnt(Integer.parseInt(param.get("orderCnt")));
 		order.setOrderSum(Integer.parseInt(param.get("orderSum")));
 		
-		int orderPk = zeroService.insertOrder(order);
+		int orderPk = sql.selectOne("orderMapper.insertOrder",order);
 		
 		//xml에서 LAST_INSERT_ID()로 가져옴
 		order.setOrderPk(orderPk);
 		
 		//order_detail 테이블에 order_pk 및 주문정보 저장하기
-		zeroService.insertOrderDetail(order);
+		sql.insert("orderMapper.insertOrderDetail",order);
 		
 		String result = "주문이 완료되었습니다.";
 		
