34a6f900f9fe4e4b3d4dbbd440652e3b2c5cca53
ladybird17 <modalon@naver.com>
2021-04-22 20:36:30 +0900

    JoinController 수정

--- src/main/java/com/bitc/zero/controller/JoinController.java
+++ src/main/java/com/bitc/zero/controller/JoinController.java
@@ -1,5 +1,6 @@
 package com.bitc.zero.controller;
 
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,9 @@ public class JoinController {
 	@Autowired
 	private ZeroService zeroService;
 	
+	@Autowired
+	protected SqlSession sql;
+	
 	//join페이지 가져오기
 	@RequestMapping(value="/zero/join", method=RequestMethod.GET)
 	public String join() throws Exception{
@@ -22,7 +26,7 @@ public class JoinController {
 	//join POST 실행하고 메인페이지로 돌아가기
 	@RequestMapping(value="/zero/join", method=RequestMethod.POST)
 	public String insertJoin(JoinDto join) throws Exception {
-		zeroService.insertJoin(join);
+		sql.insert("insertJoin",join);
 		
 		return "redirect:/zero/main";
 	}
