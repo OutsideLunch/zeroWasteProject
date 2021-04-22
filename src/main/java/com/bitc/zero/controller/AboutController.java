5e7f0d28a3952f539c1d904f72b82cb2f13c81d1
ladybird17 <modalon@naver.com>
2021-04-23 04:36:28 +0900

    about, myPage, productDetail UI 수정

--- src/main/java/com/bitc/zero/controller/AboutController.java
+++ src/main/java/com/bitc/zero/controller/AboutController.java
@@ -1,16 +1,11 @@
 package com.bitc.zero.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.bitc.zero.service.ZeroService;
-
 @Controller
 public class AboutController {
-	@Autowired
-	private ZeroService zeroService;
 	
 	//about 상세 페이지 가져오기
 	@RequestMapping(value="/zero/about", method=RequestMethod.GET)
