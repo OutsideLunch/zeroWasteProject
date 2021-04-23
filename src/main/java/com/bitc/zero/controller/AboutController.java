0a3110e1a97372eef91b13c9028037aeea05724f
ladybird17 <modalon@naver.com>
2021-04-23 15:01:41 +0900

    about, join, navbar 수정

--- src/main/java/com/bitc/zero/controller/AboutController.java
+++ src/main/java/com/bitc/zero/controller/AboutController.java
@@ -8,8 +8,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class AboutController {
 	
 	//about 상세 페이지 가져오기
-	@RequestMapping(value="/zero/about", method=RequestMethod.GET)
-	public String about() throws Exception{
-		return "/zero/about";
+	@RequestMapping(value="/zero/about1", method=RequestMethod.GET)
+	public String about1() throws Exception{
+		return "/zero/about1";
+	}
+	
+	//about 상세 페이지 가져오기
+	@RequestMapping(value="/zero/about2", method=RequestMethod.GET)
+	public String about2() throws Exception{
+		return "/zero/about2";
 	}
 }
