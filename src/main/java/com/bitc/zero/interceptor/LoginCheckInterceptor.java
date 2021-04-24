cf87d91919b4f98efa6ce9acced31ea48bb233df
JS <jinseoit@gmail.com>
2021-04-24 14:12:11 +0900

    System.out 제거

--- src/main/java/com/bitc/zero/interceptor/LoginCheckInterceptor.java
+++ src/main/java/com/bitc/zero/interceptor/LoginCheckInterceptor.java
@@ -4,28 +4,35 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.bitc.zero.controller.MypageController;
+
 
 public class LoginCheckInterceptor implements HandlerInterceptor {
+	
+	protected static final Logger logger = LogManager.getLogger(MypageController.class);
+	
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView mav) throws Exception {
 		HttpSession session = request.getSession();
 		
-//		세션에 저장된 정보 확인
+		/* 세션에 저장된 정보 확인 */
 		if((String)session.getAttribute("customerEmail") == null) {
-			System.out.println("============== interceptor ========== ====");
-			System.out.println("로그아웃 상태 : ");
-			System.out.println((String)session.getAttribute("customerEmail"));
+			logger.info("============== interceptor ========== ====");
+			logger.info("Logout Status :");
+			logger.info((String)session.getAttribute("customerEmail"));
 			
 		}
 		
 		else {
-//			10동안 세션 살림
-			System.out.println("============== interceptor ==============");
-			System.out.println("로그인 상태 : ");
-			System.out.println((String)session.getAttribute("customerEmail"));
+			/* 10동안 세션 살림 */
+			logger.info("============== interceptor ========== ====");
+			logger.info("Login Status :");
+			logger.info((String)session.getAttribute("customerEmail"));
 			
 			session.setMaxInactiveInterval(6000); //100분동안
 		}
